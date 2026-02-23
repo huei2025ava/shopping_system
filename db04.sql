14cb44e133ea9b0992e04d6b3a901f57d42f2caf
huei20251002 <avachen031@gmail.com>
2026-02-23 12:51:28 +0800

    更新最新資料庫

--- db04.sql
+++ db04.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2026-02-06 09:20:48
+-- 產生時間： 2026-02-23 05:50:46
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `db13`
+-- 資料庫： `db04`
 --
 
 -- --------------------------------------------------------
@@ -39,8 +39,9 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `acc`, `pw`, `pr`) VALUES
-(1, 'admin', '1234', 'a:5:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";i:4;s:1:\"5\";}'),
-(4, 'mack', '1357', 'a:3:{i:0;s:1:\"1\";i:1;s:1:\"4\";i:2;s:1:\"5\";}');
+(7, 'admin', '1234', 'a:5:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";i:4;s:1:\"5\";}'),
+(8, 'root', '1234', 'a:2:{i:0;s:1:\"1\";i:1;s:1:\"3\";}'),
+(9, 'apple', '1234', 'a:3:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"4\";}');
 
 -- --------------------------------------------------------
 
@@ -85,15 +86,13 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`id`, `no`, `name`, `price`, `spec`, `stock`, `img`, `intro`, `big`, `mid`, `sh`) VALUES
-(1, '510860', '', 0, '', 0, '0403.jpg', '', 1, 6, 1),
 (2, '020705', '兩用式磁扣腰包', 685, '中型', 18, '0404.jpg', '材質:進口牛皮\r\n顏色:黑色荔枝紋+黑色珠光面皮(黑色縫線)\r\n尺寸:15cm*14cm(高)*6cm(前後)\r\n產地:臺灣\r\n', 1, 5, 1),
 (3, '020706', '超薄設計男士長款真皮', 800, 'L號', 61, '0405.jpg', '基本:編織皮革對摺長款零錢包\r\n特色:最潮流最時尚的單品 \r\n顏色:黑色珠光面皮(黑色縫線)\r\n形狀:黑白格編織皮革對摺 ', 1, 5, 1),
-(4, '030103', '經典牛皮少女帆船鞋', 1000, 'S號', 6, '0406.jpg', '以傳統學院派風格聞名，創始近百年工藝製鞋精神\r\n共用獨家專利氣墊技術，兼具紐約工藝精神，與舒適跑格靈魂\r\n', 2, 7, 0),
+(4, '030103', '經典牛皮少女帆船鞋', 1000, 'S號', 6, '0406.jpg', '以傳統學院派風格聞名，創始近百年工藝製鞋精神\r\n共用獨家專利氣墊技術，兼具紐約工藝精神，與舒適跑格靈魂\r\n', 2, 7, 1),
 (5, '030203', '經典優雅時尚流行涼鞋', 2650, 'LL', 8, '0407.jpg', '優雅流線方型楦頭設計，結合簡潔線條綴飾，\r\n獨特的弧度與曲線美，突顯年輕優雅品味，\r\n是年輕上班族不可或缺的鞋款！\r\n全新美國運回，現貨附鞋盒', 2, 8, 1),
 (6, '040202', '寵愛天然藍寶女戒', 28000, '1克拉', 1, '0408.jpg', '◎典雅設計品味款\r\n◎藍寶為珍貴天然寶石之一，具有保值收藏\r\n◎專人設計製造，以貴重珠寶精緻鑲工製造\r\n', 3, 10, 1),
-(7, '050107', '反折式大容量手提肩背包', 888, 'L號', 15, '0409.jpg', '特色:反折式的包口設計,釘釦的裝飾,讓簡單的包型更增添趣味性\r\n材質:棉布\r\n顏色:藍色\r\n尺寸:長50cm寬20cm高41cm\r\n產地:日本\r\n', 4, 11, 0),
-(8, '060108', '男單肩包男', 650, '多功能', 7, '', '特色:男單肩包/電腦包/公文包/雙肩背包多用途\r\n材質:帆不\r\n顏色:黑色\r\n尺寸:深11cm寬42cm高33cm\r\n產地:香港\r\n', 4, 11, 0),
-(10, '204037', '木尸日火日', 12313, '日火尸木日', 12, '0410.jpg', 'dsfasdfas\r\ndsaffasf\r\nsadfasd', 1, 5, 1);
+(7, '050107', '反折式大容量手提肩背包', 888, 'L號', 15, '0409.jpg', '特色:反折式的包口設計,釘釦的裝飾,讓簡單的包型更增添趣味性\r\n材質:棉布\r\n顏色:藍色\r\n尺寸:長50cm寬20cm高41cm\r\n產地:日本\r\n', 4, 11, 1),
+(8, '060108', '男單肩包男', 650, '多功能', 7, '', '特色:男單肩包/電腦包/公文包/雙肩背包多用途\r\n材質:帆不\r\n顏色:黑色\r\n尺寸:深11cm寬42cm高33cm\r\n產地:香港\r\n', 4, 11, 1);
 
 -- --------------------------------------------------------
 
@@ -117,7 +116,34 @@ CREATE TABLE `members` (
 --
 
 INSERT INTO `members` (`id`, `acc`, `pw`, `name`, `tel`, `address`, `email`, `reg_date`) VALUES
-(1, 'mem01', 'mem01', 'mem07', '123123123', 'dsfasdfsdaf', 'dfasdfdas@dsfasdfsa', '2026-02-02');
+(2, 'mem01', '1234', 'amy', '0988123456', '456', '456@gmail.com', '2026-02-03'),
+(3, 'aaa', '1234', 'aaa', 'aaa', 'ccc', 'vvvv', '2026-02-03');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `no` text NOT NULL,
+  `acc` text NOT NULL,
+  `name` text NOT NULL,
+  `tel` text NOT NULL,
+  `address` text NOT NULL,
+  `email` text NOT NULL,
+  `sum` int(10) UNSIGNED NOT NULL,
+  `cart` text NOT NULL,
+  `order_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `orders`
+--
+
+INSERT INTO `orders` (`id`, `no`, `acc`, `name`, `tel`, `address`, `email`, `sum`, `cart`, `order_date`) VALUES
+(1, '20260223975904', 'aaa', 'aaa', 'aaa', 'ccc', 'vvvv', 143885, 'a:3:{i:2;s:1:\"1\";i:3;s:1:\"4\";i:6;s:1:\"5\";}', '2026-02-23');
 
 -- --------------------------------------------------------
 
@@ -126,10 +152,10 @@ INSERT INTO `members` (`id`, `acc`, `pw`, `name`, `tel`, `address`, `email`, `re
 --
 
 CREATE TABLE `types` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` text NOT NULL,
-  `big_id` int(10) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id` int(11) UNSIGNED NOT NULL COMMENT '自增ID',
+  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '分類名稱',
+  `big_id` int(11) NOT NULL DEFAULT 0 COMMENT '所屬大分類ID (0表示大分類)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- 傾印資料表的資料 `types`
@@ -146,8 +172,7 @@ INSERT INTO `types` (`id`, `name`, `big_id`) VALUES
 (8, '紳士流行鞋區', 2),
 (9, '時尚手錶', 3),
 (10, '時尚珠寶', 3),
-(11, '背包', 4),
-(14, '', 0);
+(11, '背包', 4);
 
 --
 -- 已傾印資料表的索引
@@ -178,6 +203,12 @@ ALTER TABLE `members`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `types`
 --
 ALTER TABLE `types`
@@ -191,7 +222,7 @@ ALTER TABLE `types`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `bot`
@@ -203,19 +234,25 @@ ALTER TABLE `bot`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `members`
 --
 ALTER TABLE `members`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `orders`
+--
+ALTER TABLE `orders`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `types`
 --
 ALTER TABLE `types`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增ID', AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
